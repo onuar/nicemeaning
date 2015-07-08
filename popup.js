f6633ccac7a3e90684489f1055f53af714936b1d
onuraykac@gmail.com <yineParam9213#>
2015-07-08 14:44:39 +0300

    issue#2

--- popup.js
+++ popup.js
@@ -1,20 +1,19 @@
 $(document).ready(function() {
+	$("#translate").focus();
+
+	chrome.tabs.getSelected(null, function(tab) {
+		chrome.tabs.sendRequest(tab.id, {action:"getSelectedText"}, function(response){
+			$("#word").val(response.selectedText);
+		});
+	});
+
 	$("#translate").click(function(){
 		var $word = $("#word");
 		var text = $word.val();
 		if(text){
 			translater.get(text, 12, function(translatedData){
-				$word.val(translatedData["text"]);
+				$("#result").val(translatedData["text"]);
 			});
 		}
-	});
-
-	function onSelectedText(response){
-		alert(encodeURIComponent(response.selectedText));
-	};
-
-	chrome.tabs.getCurrent(null, function(tab) {
-		chrome.tabs.sendRequest(tab.id, {action: "getSelectedText"}, function(response){
-		});
 	});
 });
